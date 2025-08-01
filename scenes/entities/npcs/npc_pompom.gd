a9e60fff9cde67034d71193e2d930f9f7a643528
Rioni <nrosa01@ucm.es>
2025-08-01 17:17:48 +0200

    Basic logic to pull allies

--- scenes/entities/npcs/npc_pompom.gd
+++ scenes/entities/npcs/npc_pompom.gd
@@ -10,6 +10,10 @@ var is_immobilised = false
 const movement_wait_times = [3.0, 5.0, 7.0]
 const speed = 30
 
+var being_pulled := false
+@export var pull_force := .4
+@export var distance_influence := .4
+
 func _ready() -> void:
 	isAlly = true;
 
@@ -17,9 +21,15 @@ func _ready() -> void:
 # Helped by Godot docs: https://docs.godotengine.org/en/stable/tutorials/2d/2d_movement.html#click-and-move
 # And Godot forums: https://forum.godotengine.org/t/how-to-make-an-area2d-apears-on-random-position-in-the-screen/20456/2
 func _physics_process(_delta: float) -> void:
-	var health_modifier = 1.0 - 0.25 * rope_state
-	velocity = position.direction_to(target) * speed * health_modifier
-	if position.distance_to(target) > 10:
+	if !being_pulled:
+		var health_modifier = 1.0 - 0.25 * rope_state
+		velocity = velocity.lerp(position.direction_to(target) * speed * health_modifier, .08)
+		if position.distance_to(target) > 10:
+			move_and_slide()
+	else:
+		if Input.is_action_just_released("pointer_interaction"): being_pulled = false
+		
+		velocity += global_position.direction_to(get_global_mouse_position()) * pull_force * (distance_influence * global_position.distance_to(get_global_mouse_position()))
 		move_and_slide()
 
 func _on_timer_movement_timeout() -> void:
@@ -70,8 +80,9 @@ func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 				is_immobilised = true
 				# TODO: Handle the capturing game mechanic. For now, clicking a tied-up pompom will capture them
 				SignalBus.pompom_capture.emit(100)
-				print(name + ": *was Thanos snapped*")
-				queue_free()
+				being_pulled = true
+				#print(name + ": *was Thanos snapped*")
+				#queue_free()
 			else:
 				print(name + ": Hehe, you can't capture me yet!")
 				$Sprite2D/temp_capturefailgiggle.visible = true
