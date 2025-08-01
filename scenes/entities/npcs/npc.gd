c0b24f192f150eb516149350f546cc11a0bc4fc1
CrossfireCam <30458404+crossfirecam@users.noreply.github.com>
2025-08-01 15:50:01 +1000

    Enemy: Basic randomised movement
    - Change NPC to extend from CharacterBody2D, as it's better built for user-controlled physics bodies (as the Godot forums explained to me, this doesn't JUST mean character controllers for the player. User-controlled refers to any physics body that the dev wants to move around via code)
    - Enemy chooses a random target on screen, then moves toward it
    - When a timer expires per enemy, it'll choose a new target, and reset the timer to a random value (out of an array of options: `movement_wait_times = [3.0, 5.0, 7.0]`)
    - Lasso'ing an enemy slows them down in 25% increments

--- scenes/entities/npcs/npc.gd
+++ scenes/entities/npcs/npc.gd
@@ -1,4 +1,17 @@
-extends StaticBody2D
+extends CharacterBody2D
 class_name NPC
 
 var isAlly = false;
+
+@onready var screenSize = get_viewport().get_visible_rect().size
+var target = position
+
+func _choose_new_destination():
+	var rng = RandomNumberGenerator.new()
+	var rndX = rng.randi_range(0, screenSize.x)
+	var rndY = rng.randi_range(0, screenSize.y)
+	target = Vector2(rndX, rndY)
+
+func _choose_from_array(array):
+	array.shuffle()
+	return array.front()
