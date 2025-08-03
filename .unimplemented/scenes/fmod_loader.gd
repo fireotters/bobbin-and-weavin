8867f2c6649a0ab6c91ce0ea94b58ad8503704cb
Rioni <nrosa01@ucm.es>
2025-08-03 12:39:07 +0200

    Removed fmod plugin that wasn't being used

--- .unimplemented/scenes/fmod_loader.gd
+++
@@ -1,7 +0,0 @@
-extends Node
-
-var banks := Array()
-
-func _ready() -> void:
-	banks.append(FmodServer.load_bank("res://FMOD/Desktop/Master.strings.bank", FmodServer.FMOD_STUDIO_LOAD_BANK_NORMAL))
-	banks.append(FmodServer.load_bank("res://FMOD/Desktop/Master.bank", FmodServer.FMOD_STUDIO_LOAD_BANK_NORMAL))
